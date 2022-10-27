38f9c19c045a0d6bbcb2457a502ca557ebabb30c
LeonardoGasperin <gasperinlvg@gmail.com>
2022-10-27 16:08:54 -0300

    Exercicies 1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const course = "FullstackOpen course application development";
+  const part1 = "Introduction to React";
+  const exercicie1 = 10;
+  const part2 = "Comunication with a server";
+  const exercicie2 = 7;
+  const part3 = "Programming a server with nodeJS and Express";
+  const exercicie3 = 14;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>{part1} {exercicie1}</p>
+      <p>{part2} {exercicie2}</p>
+      <p>{part3} {exercicie3}</p>
+      <p>Number of exercicies {exercicie1 + exercicie2 + exercicie3}</p>
     </div>
   );
 }
